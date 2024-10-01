11d7d280f6290469c26862244647e8780e43fc33
Plataforma de Telemedicina com Acessibilidade Avançada <joao176069@gmail.com>
2024-10-01 16:50:31 +0000

    Salvar codigo 3

---
+++ Desenvolvimento.py
@@ -0,0 +1,21 @@
+import os 
+from dataclasses import dataclass
+
+os.system("cls || clear")
+
+@dataclass
+class Pesquisa :
+    total_de_familias : int
+    media_salario_populacao : float
+    media_numero_de_filhos : int 
+    maior_salario : float
+    menor_salario : float
+
+PESQUISA_DIGITALIZADA = int (input ("Digite a quantidade de familias"))
+lista_de_clientes = []
+
+for i in range (PESQUISA_DIGITALIZADA):
+
+    total_de_familias = int (input ("Digite o total de familias"))
+    media_de_num
+    
