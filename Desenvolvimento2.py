d74e4e9c9e12861384f3b66fe941969975941924
Bruno Henrique Alves Santos <bhas795001@gmail.com>
2024-10-01 17:44:43 +0000

    salvar 8

--- Desenvolvimento2.py
+++ Desenvolvimento2.py
@@ -19,7 +19,10 @@ def maior_menor():
 
 lista_de_familias = []
 lista_de_salarios = []
+familias = []
 
+numero_ee_familias
+contador_salario = len(lista_de_salarios)
 
 while True:
     print("""
@@ -29,18 +32,22 @@ while True:
     |   2   | Exibir resultados |          
     |   3   | Sair              |  
     """)
-    menu = int(input("Digite uma opção"))
+    menu = int(input("Digite uma opção: "))
+    os.system("cls || clear")
     match menu:
         case 1:
            #solicitando dados
            familia = Pesquisa(
-               nome = input("Digite seu nome"),
+               nome = input("Digite seu nome: "),
                numero_de_filhos_por_familia = int(input("digite o números de filhos na sua familia: ")),
                salario = float(input("Digite seu salário: "))
            )
            lista_de_salarios.append({familia.salario})
            lista_de_familias.append({familia.numero_de_filhos_por_familia})
-           os.system()
+           os.system("cls || clear")
+           with open(nome_do_arquivo, "a") as pesquisa:
+                for clthanos in familia:
+                    pesquisa.write(f"{clthanos.nome}, {clthanos.sobrenome}, {clthanos.idade}, {clthanos.peso},{clthanos.altura}\n")
         case 2 :
             #exibindo dados
             print("\n=== Exibindo dados ===")
@@ -50,9 +57,10 @@ while True:
                 print(f"Idade: {clthanos.idade} ")
                 print(f"Seso: {clthanos.peso} ")
                 print(f"Altura: {clthanos.altura} ")
-            with open(nome_do_arquivo, "a") as pesquisa:
-                for clthanos in familia:
-                    pesquisa.write(f"{clthanos.nome}, {clthanos.sobrenome}, {clthanos.idade}, {clthanos.peso},{clthanos.altura}\n")
+            with open (nome_do_arquivo, "r") as todos_alunos:
+                for linha in familia:
+                  nome,sobrenome,idade,peso, altura = linha.strip().split(",")
+                  familias.append(pesquisa(nome=nome,salario=int(), idade=int(idade),peso = float(peso), altura=float(altura)))
         case 3:
             #parando o codigo
             pesquisa.close()
